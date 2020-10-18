e31ef7c4147bd2e9b18852ed68271a3a3decbce6
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-10-18 19:28:39 +0200

    #2-#4

---
+++ VaR/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaR
+{
+    public partial class Form1 : Form
+    {
+
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+    }
+}
