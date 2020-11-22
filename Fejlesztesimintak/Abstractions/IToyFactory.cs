83c0cf4a260424c323313e1f18451d5be914abb3
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-11-22 22:14:35 +0100

    #8

---
+++ Fejlesztesimintak/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fejlesztesimintak.Entities;
+
+namespace Fejlesztesimintak.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
