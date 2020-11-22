93d63f4624508df48cf116c9d868777963d1804b
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-11-22 21:53:05 +0100

    #4

--- Fejlesztesimintak/Entities/Ballfactory.cs
+++ Fejlesztesimintak/Entities/Ballfactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Fejlesztesimintak.Entities
 {
-    public class Ballfactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
--- Fejlesztesimintak/Form1.Designer.cs
+++ Fejlesztesimintak/Form1.Designer.cs
@@ -30,12 +30,46 @@ namespace Fejlesztesimintak
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.createTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.mainPanel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // createTimer
+            // 
+            this.createTimer.Enabled = true;
+            this.createTimer.Interval = 3000;
+            this.createTimer.Tick += new System.EventHandler(this.createTimer_Tick_1);
+            // 
+            // conveyorTimer
+            // 
+            this.conveyorTimer.Enabled = true;
+            this.conveyorTimer.Interval = 10;
+            this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick_1);
+            // 
+            // mainPanel1
+            // 
+            this.mainPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel1.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel1.Name = "mainPanel1";
+            this.mainPanel1.Size = new System.Drawing.Size(811, 422);
+            this.mainPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(811, 422);
+            this.Controls.Add(this.mainPanel1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        public System.Windows.Forms.Timer createTimer;
+        public System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Panel mainPanel1;
     }
 }
 
