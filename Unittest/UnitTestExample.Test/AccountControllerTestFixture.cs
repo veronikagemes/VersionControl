b87726e7ef47f4249859d06cd887c05a12c8cf92
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-11-29 20:31:11 +0100

    #4

---
+++ Unittest/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // ...
+        }
+    }
+}
