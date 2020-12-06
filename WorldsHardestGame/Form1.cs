3a85e6fd193d303c3ee451e39e020797cc1afeaa
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-12-06 21:31:18 +0100

    #5

--- WorldsHardestGame/Form1.cs
+++ WorldsHardestGame/Form1.cs
@@ -30,6 +30,25 @@ namespace WorldsHardestGame
             //gc.AddPlayer();
             //gc.Start(true);
 
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
     }
 }
