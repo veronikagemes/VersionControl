ee18a88f6ae15b91feee135ff86378ad23448b75
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-12-13 20:22:48 +0100

    #7

--- Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Form1.cs
@@ -25,10 +25,7 @@ namespace Webszolgaltatas
             InitializeComponent();
             dataGridView1.DataSource = Rates;
 
-
-            harom();
-            ot();
-            hat();
+            RefreshData();
         }
 
         void harom() 
@@ -37,9 +34,9 @@ namespace Webszolgaltatas
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString(),
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -88,6 +85,29 @@ namespace Webszolgaltatas
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        void RefreshData() 
+        {
+            Rates.Clear();
+            harom();
+            ot();
+            hat();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
