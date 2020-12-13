fa0d644a9d55607e486d6408678cd01a4545083e
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-12-13 20:54:37 +0100

    #5

--- Mikroszimulacio/Mikroszimulacio/Form1.cs
+++ Mikroszimulacio/Mikroszimulacio/Form1.cs
@@ -14,6 +14,7 @@ namespace Mikroszimulacio
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
