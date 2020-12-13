9dd0e9868cf3983e8a59b871844486a0189e0843
Veronika Gémes <veronika.gemes@stud.uni-corvinus.hu>
2020-12-13 20:45:54 +0100

    #3

---
+++ Mikroszimulacio/Mikroszimulacio/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public class BirthProbability
+    {
+        public int Kor { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ Mikroszimulacio/Mikroszimulacio/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Kor { get; set; }
+        public double P { get; set; }
+    }
+}
